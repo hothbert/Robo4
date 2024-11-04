1ca460bb93deb2746407f624f45490d281e2482f
ParkinsonThomas <u05tp21@abdn.ac.uk>
2024-11-04 14:45:10 +0000

    Worlds file created, launch file partial.

---
+++ robo4_project/launch/robo4_project_launch.py
@@ -0,0 +1,10 @@
+import os
+import launch
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from webots_ros2_driver.webots_launcher import WebotsLauncher
+from webots_ros2_driver.webots_controller import WebotsController
+
+def generate_launch_description():
+    package_dir = get_package_share_directory('robo4_project')
+    robot_description_path = os.path.join(package_dir)
