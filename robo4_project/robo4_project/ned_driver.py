1ad8ee7255800f74249d586ab557b59e70035efd
ParkinsonThomas <u05tp21@abdn.ac.uk>
2024-11-06 12:33:58 +0000

    Setup.py and ned_driver completed.

--- robo4_project/robo4_project/ned_driver.py
+++ robo4_project/robo4_project/ned_driver.py
@@ -3,3 +3,10 @@ import rclpy
 class NedRobotDriver:
     def init(self, webots_node, properties):
         self.__robot = webots_node.robot
+
+def main():
+    rclpy.init()
+    driver = NedRobotDriver()
+    rclpy.spin(driver)
+    driver.destroy_node()
+    rclpy.shutdown()
--- robo4_project/setup.py
+++ robo4_project/setup.py
@@ -1,16 +1,18 @@
 from setuptools import find_packages, setup
 
 package_name = 'robo4_project'
+data_files = []
+data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
+data_files.append(('share/' + package_name + '/launch', ['launch/robo4_project_launch.py']))
+data_files.append(('share/' + package_name + '/worlds', ['worlds/robo4_world.wbt']))
+data_files.append(('share/' + package_name + '/resource', ['resource/ned.urdf']))
+data_files.append(('share/' + package_name, ['package.xml']))
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='thomas',
@@ -20,7 +22,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'node1 = robo4_project.node1:main'
+            'ned_driver = robo4_project.ned_driver:main'
         ],
     },
 )
