893d13c73a4395acac0c68df1ca0d0aa3e9d7d0f
hothbert <holly.sinclair2004@gmail.com>
2024-11-18 15:18:24 +0000

    added robot description to robot

--- robo4_project/launch/robo4_project_launch.py
+++ robo4_project/launch/robo4_project_launch.py
@@ -10,10 +10,19 @@ from webots_ros2_driver.webots_controller import WebotsController
 
 def generate_launch_description():
     package_dir = get_package_share_directory('robo4_project')
-    robot_description_path = pathlib.Path(os.path.join(package_dir, 'resource', 'panda.urdf')).read_text()
+    robot_description_path = pathlib.Path(os.path.join(package_dir, 'resource', 'demo.urdf')).read_text()
+    controller_path = os.path.join(package_dir, 'resource', 'panda_joints.yaml')
 
     webots = WebotsLauncher(
         world = os.path.join(package_dir, 'worlds','robo4_world.wbt')
+    )
+
+    ros2_control_node = Node(
+        package = 'controller_manager',
+        executable ='ros2_control_node',
+        name = 'ros2_control_node',
+        parameters=[{'robot_description': robot_description_path}, controller_path],
+        output='screen'
     )
 
     panda_driver = WebotsController(
@@ -41,6 +50,7 @@ def generate_launch_description():
     return LaunchDescription([
         webots,
         panda_driver,
+        ros2_control_node,
         panda_sensor,
         panda_camera,
         panda_sort,
--- robo4_project/robo4_project/panda_sort.py
+++ robo4_project/robo4_project/panda_sort.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 from sensor_msgs.msg import Range
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
 #this should serve as our controller node
 
@@ -11,12 +12,21 @@ class PandaSort(Node):
 
         self.create_subscription(Range, '/distance_msg', self.get_distance, 10)
         self.create_subscription(String, '/colour_msg', self.move_block, 10)
+
+        self.publisher = self.create_publisher(JointTrajectory, '/panda/joint_trajectory_controller/command', 10)
         
     def move_block(self, msg):
         colour_value = msg.data
         if colour_value in ('blue', 'green', 'cyan'):
             self.get_logger().info(f"{colour_value} block detected!")
-            #open gripper
+            
+            movement = JointTrajectory()
+            movement.joint_names = ['panda_joint1', 'panda_joint2']
+            point = JointTrajectoryPoint()
+            point.positions = [0.6, 0.6]
+            movement.points.append(point)
+            self.publisher.publish(movement)
+
         else:
             self.get_logger().info("waiting for block...")
 
