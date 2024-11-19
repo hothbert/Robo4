e59852543ff5e4bec1dd20d9a2a6b90f282eee37
hothbert <holly.sinclair2004@gmail.com>
2024-11-19 21:09:31 +0000

    left comment instructions for haziel and fariha

--- robo4_project/robo4_project/panda_driver.py
+++ robo4_project/robo4_project/panda_driver.py
@@ -4,6 +4,13 @@ from webots_ros2_driver.webots_controller import WebotsController
 class PandaRobotDriver(WebotsController):
     def init(self, webots_node, properties):
         self.__robot = webots_node.robot
+        #define the gripper joints and all other relevant ones
+        #panda_finger_joint1  etc...
+
+        #subscribe to commands coming from panda sort
+
+    #define functions that act on these commands and execute movement
+    # good luck    
 
     def step(self):
         pass
--- robo4_project/robo4_project/panda_sort.py
+++ robo4_project/robo4_project/panda_sort.py
@@ -3,21 +3,29 @@ from rclpy.node import Node
 from std_msgs.msg import String
 from sensor_msgs.msg import Range
 
-#this should serve as our controller node
+#this should serve as our node for moving things idk
 
 class PandaSort(Node):
     def __init__(self):
         super().__init__('panda_sort')
-
+        #subscribes to the nodes collecting sensor data.
         self.create_subscription(Range, '/distance_msg', self.get_distance, 10)
         self.create_subscription(String, '/colour_msg', self.move_block, 10)
-
+        
+        #publish commands or something here
         
     def move_block(self, msg):
+        #i think idealy, we stop the conveyer belt from moving to make it easier to pick up
+        #might need to re-align the sensors to make this easier and more precise
         colour_value = msg.data
         if colour_value in ('blue', 'green', 'cyan'):
             self.get_logger().info(f"{colour_value} block detected!")
-
+            #send open gripper command
+            #bend down and pick up block
+            #close gripper
+            #if its blue turn to left and drop that shit on the floor
+            #right for cyan
+            #a third direction for green
         else:
             self.get_logger().info("waiting for block...")
 
