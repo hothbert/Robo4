c84d5b9fb8fd8e1cac1d19cfdbccfe839931a594
hothbert <holly.sinclair2004@gmail.com>
2024-11-23 00:20:48 +0000

    I DID IT

--- robo4_project/robo4_project/panda_driver.py
+++ robo4_project/robo4_project/panda_driver.py
@@ -3,15 +3,17 @@ from rclpy.node import Node
 from webots_ros2_driver.webots_controller import WebotsController
 from std_msgs.msg import String
 
-class PandaRobotDriver(WebotsController):
+class PandaRobotDriver(WebotsController, Node):
     def init(self, webots_node, properties):
-        super().__init__(webots_node, properties)
+        rclpy.init()
+        self.node = Node('panda_driver')
         self.__robot=webots_node.robot
+        
 
-        self.gripper_left = self.__robot.getDevice('panda_finger_joint1')  # Adjust with actual motor name
-        self.gripper_right = self.__robot.getDevice('panda_finger_joint2')  # Adjust with actual motor name
+        self.gripper_left = self.__robot.getDevice('panda_joint1')  # Adjust with actual motor name
+        self.gripper_right = self.__robot.getDevice('panda_joint2')  # Adjust with actual motor name
 
-        self.gripper_cmd_sub = self.create_subscription(
+        self.gripper_cmd_sub = self.node.create_subscription(
             String,  # Replace with your chosen message type
             '/gripper_command',  # Topic name
             self.gripper_command_callback,
@@ -28,18 +30,18 @@ class PandaRobotDriver(WebotsController):
     def gripper_command_callback(self, msg):
         command = msg.data
         if command == "open":
-            self.gripper_left.setPosition(0.04)  
-            self.gripper_right.setPosition(0.04)
-            self.get_logger().info("Gripper opened.")
+            self.gripper_left.setPosition(2)  
+            self.gripper_right.setPosition(2)
+            self.node.get_logger().info("Gripper opened.")
         #elif command == "close":
             #self.gripper_left.setPosition(0.0)  
             #self.gripper_right.setPosition(0.0)
             #self.get_logger().info("Gripper closed.")
         else:
-            self.get_logger().warn(f"Unknown gripper command: {command}")   
+            self.node.get_logger().warn(f"Unknown gripper command: {command}")   
 
     def step(self):
-        pass
+        rclpy.spin_once(self.node, timeout_sec=0)
     #control logic/sensor updates here
 
 def main():
