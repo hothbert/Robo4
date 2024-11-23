50842afbfeb13008c798216db0d9f9865ca165b7
hothbert <holly.sinclair2004@gmail.com>
2024-11-23 00:53:10 +0000

    open gripper

--- robo4_project/robo4_project/panda_driver.py
+++ robo4_project/robo4_project/panda_driver.py
@@ -6,33 +6,25 @@ from std_msgs.msg import String
 class PandaRobotDriver(WebotsController, Node):
     def init(self, webots_node, properties):
         rclpy.init()
-        self.node = Node('panda_driver')
-        self.__robot=webots_node.robot
+        self.node = Node('panda_driver') #creates a node to access ros2 functions
+        self.__robot=webots_node.robot 
         
 
-        self.gripper_left = self.__robot.getDevice('panda_joint1')  # Adjust with actual motor name
-        self.gripper_right = self.__robot.getDevice('panda_joint2')  # Adjust with actual motor name
+        self.gripper_left = self.__robot.getDevice('panda_finger::left')
+        self.gripper_right = self.__robot.getDevice('panda_finger::right')
+        self.bend_down = self.__robot.getDevice('panda_joint1')
 
-        self.gripper_cmd_sub = self.node.create_subscription(
-            String,  # Replace with your chosen message type
-            '/gripper_command',  # Topic name
-            self.gripper_command_callback,
-            10
-        )
-
-        #define the gripper joints and all other relevant ones
-        #panda_finger_joint1  etc...
-
-        #subscribe to commands coming from panda sort
+        self.gripper_cmd_sub = self.node.create_subscription(String, '/gripper_command', self.gripper_command_callback, 10)
 
     #define functions that act on these commands and execute movement
     # good luck 
     def gripper_command_callback(self, msg):
         command = msg.data
         if command == "open":
-            self.gripper_left.setPosition(2)  
-            self.gripper_right.setPosition(2)
+            self.gripper_left.setPosition(0.04)  
+            self.gripper_right.setPosition(0.04)
             self.node.get_logger().info("Gripper opened.")
+            
         #elif command == "close":
             #self.gripper_left.setPosition(0.0)  
             #self.gripper_right.setPosition(0.0)
@@ -42,7 +34,7 @@ class PandaRobotDriver(WebotsController, Node):
 
     def step(self):
         rclpy.spin_once(self.node, timeout_sec=0)
-    #control logic/sensor updates here
+
 
 def main():
     rclpy.init()
