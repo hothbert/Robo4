022e17b54b26db3b46b5d6fb5bcd6128842c0d6b
hothbert <holly.sinclair2004@gmail.com>
2024-11-23 23:45:56 +0000

    tidied up and added comments to explain

--- robo4_project/robo4_project/panda_driver.py
+++ robo4_project/robo4_project/panda_driver.py
@@ -3,18 +3,23 @@ from rclpy.node import Node
 from webots_ros2_driver.webots_controller import WebotsController
 from std_msgs.msg import String
 
-class PandaRobotDriver(WebotsController, Node):
+#this serves as an interface between ros2 nodes and the webots robot
+#its soul purpose is to send positions to each joint
+#it subscribes to robot movement commands and acts upon them
+#lets not put any logic here, save that for the actual ros2 nodes
+
+class PandaRobotDriver(WebotsController):
     def init(self, webots_node, properties):
+        #creates a node so that rclpy features can be used, since PandaRobotDriver doesnt inherit from Node
         rclpy.init()
-        self.node = Node('panda_driver') #creates a node to access ros2 functions
+        self.node = Node('panda_driver')
         self.__robot=webots_node.robot 
-        
 
         self.gripper_left = self.__robot.getDevice('panda_finger::left')
         self.gripper_right = self.__robot.getDevice('panda_finger::right')
         self.bend_down = self.__robot.getDevice('panda_joint1')
 
-        self.gripper_cmd_sub = self.node.create_subscription(String, '/gripper_command', self.gripper_command_callback, 10)
+        self.node.create_subscription(String, '/gripper_command', self.gripper_command_callback, 10)
 
     #define functions that act on these commands and execute movement
     # good luck 
@@ -29,17 +34,7 @@ class PandaRobotDriver(WebotsController, Node):
             #self.gripper_left.setPosition(0.0)  
             #self.gripper_right.setPosition(0.0)
             #self.get_logger().info("Gripper closed.")
-        else:
-            self.node.get_logger().warn(f"Unknown gripper command: {command}")   
+
 
     def step(self):
-        rclpy.spin_once(self.node, timeout_sec=0)
-
-
-def main():
-    rclpy.init()
-    driver = PandaRobotDriver()
-    rclpy.spin(driver)
-    driver.destroy_node()
-    rclpy.shutdown()
-
+        rclpy.spin_once(self.node, timeout_sec=0) #starts self.ynode so that subscription works
--- robo4_project/robo4_project/panda_sort.py
+++ robo4_project/robo4_project/panda_sort.py
@@ -2,60 +2,64 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 from sensor_msgs.msg import Range
+#from robo4_project.msg import JointMove
 
 
-#this should serve as our node for moving things idk
+#this should serve as our node for controlling moving things
+#todo: bend down, close gripper, stand up, turn to direction, open gripper to release block
 
 class PandaSort(Node):
     def __init__(self):
         super().__init__('panda_sort')
         #subscribes to the nodes collecting sensor data.
-        self.create_subscription(Range, '/distance_msg', self.get_distance, 10)
-        self.create_subscription(String, '/colour_msg', self.move_block, 10)
-        
-        #publish commands or something here
+        self.colour = None
+        self.create_subscription(Range, '/distance_msg', self.move_block, 10)
+        self.create_subscription(String, '/colour_msg', self.get_colour, 10)
+
+        #for other joints, we could create custom message to specify joint and how much it moves by
         self.publisher = self.create_publisher(String, '/gripper_command', 10)
+        #self.joint_publisher = self.create_publisher(JointMove, '/joint_move', 10)   
 
     def open_gripper(self):
         msg = String()
         msg.data = 'open'
         self.publisher.publish(msg)
         self.get_logger().info("Gripper open command sent.")
-
-    #def close_gripper(self):
-        #msg = String()
-        #msg.data = 'close'
-        #self.publisher.publish(msg)
-        #self.get_logger().info("Gripper close command sent.")
         
-    def move_block(self, msg):
-        #i think idealy, we stop the conveyer belt from moving to make it easier to pick up
-        #might need to re-align the sensors to make this easier and more precise
+    def get_colour(self, msg):
         colour_value = msg.data
         if colour_value in ('blue', 'green', 'cyan'):
-            self.get_logger().info(f"{colour_value} block detected!")
-            #send open gripper command
-            self.open_gripper()
-            
-            #bend down and pick up block
-            #close gripper
-            #self.close_gripper()
-            
-            #if its blue turn to left and drop that shit on the floor
-            if colour_value == 'blue':
-                self.get_logger().info("Moving block to the left.")
-            elif colour_value == 'cyan':
-                self.get_logger().info("Moving block to the right.")
-            elif colour_value == 'green':
-                self.get_logger().info("Moving block forward.")
-            #right for cyan
-            #a third direction for green
+            self.colour = colour_value
+            self.get_logger().info(f"{self.colour} block detected!")
         else:
-            self.get_logger().info("waiting for block...")
+            self.colour = None
 
-    def get_distance(self, msg):
+    def move_block(self, msg):
+        #we may need to pause between movements so that the robot doesnt try and do all movements at once
+        #or work out how the rclpy spin stuff works so that it does this once per detected block
+        #might need to realign sensors or slow stuff down to make stuff more precise
         distance = msg.range
         self.get_logger().info(f"detected distance: {distance}")
+        if distance < 0.5:
+            self.open_gripper()
+            block_colour = self.colour
+            #bend down by [distance] meters
+            #close gripper
+            #stand up
+            if block_colour is not None:
+                if block_colour == 'blue':
+                    self.get_logger().info("Moving block to the left.")
+                    #move left
+                    #open gripper
+                    #move back
+                elif block_colour == 'cyan':
+                    self.get_logger().info("Moving block to the right.")
+                    #same as above
+                elif block_colour == 'green':
+                    self.get_logger().info("Moving block forward.")
+            #close gripper        
+        else:
+            self.get_logger().info("waiting for block...")
 
 def main():
     rclpy.init()
